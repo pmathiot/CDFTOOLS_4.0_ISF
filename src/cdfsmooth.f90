59180a33b0bd6f1aa0be2b7e6ac08858d82a356a
pmathiot <pierre.mathiot@metoffice.gov.uk>
2017-11-06 17:46:54 +0000

    correct normalisation in the hanning filter close to the coastline

--- src/cdfsmooth.f90
+++ src/cdfsmooth.f90
@@ -370,8 +370,8 @@ CONTAINS
     dec2d(0,1) = 1.d0 ;  dec2d(2,1) = 1.d0
 
     ! normalize
-    dl_sum     = SUM(dec2d)
-    dec2d(:,:) = dec2d(:,:) / dl_sum
+    !dl_sum     = SUM(dec2d)
+    !dec2d(:,:) = dec2d(:,:) / dl_sum
 
   END SUBROUTINE inithann
 
@@ -524,7 +524,8 @@ CONTAINS
 
              ! we don't compute in land
              IF ( kiw(ji,jj) == 1 ) THEN
-                py(ji,jj) = SUM( dec2d(:,:) * ztmp(iiminus1:iiplus1,ijminus1:ijplus1) )
+                py(ji,jj) = SUM( dec2d(:,:) * ztmp(iiminus1:iiplus1,ijminus1:ijplus1) * kiw(iiminus1:iiplus1,ijminus1:ijplus1) )
+                py(ji,jj) = py(ji,jj) / SUM( dec2d(:,:) * kiw(iiminus1:iiplus1,ijminus1:ijplus1) )   ! normalisation
              ENDIF
           ENDDO
        ENDDO
